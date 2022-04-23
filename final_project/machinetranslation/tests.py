db2e33b142bb89fe8e80e0f75a11272c56a72ce1
Dr. Robi Mostafa <101678736+robihsnm@users.noreply.github.com>
2022-04-23 18:34:34 -0400

    Update tests.py

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,24 +1,25 @@
 import unittest
-from translator import french_to_english
-from translator import english_to_french
-class TestMyModule1(unittest.TestCase):
-    def test_f2e1(self):
-        self.assertEqual(french_to_english('Au revoir'), 'Goodbye')
-    def test_f2e2(self):
-        self.assertEqual(french_to_english('Bonjour'), 'Hello')
-    def test_f2e3(self):
-        self.assertNotEqual(french_to_english('Oui'), 'No')
-    def test_f2e4(self):
-        self.assertNotEqual(french_to_english(0), 0)
 
-class TestMyModule2(unittest.TestCase):
-    def test_e2f1(self):
-        self.assertEqual(english_to_french('Hello'), 'Bonjour')
-    def test_e2f2(self):
-        self.assertEqual(english_to_french('Goodbye'), 'Au revoir')
-    def test_e2f3(self):
-        self.assertNotEqual(english_to_french('Hello'),'Au revoir')
-    def test_e2f4(self):
+from translator import english_to_french 
+from translator import french_to_english
+
+class test_english_to_french(unittest.TestCase):
+    def test_en2fr(self):
         self.assertNotEqual(english_to_french(0), 0)
+        # test when null is given as input the output is null.
+        self.assertNotEqual(english_to_french('null'), '')
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        # test when word is given as input the output is translated.
+        self.assertEqual(english_to_french('Road'), 'Route')
+
+class test_french_to_english(unittest.TestCase):
+    def test_fr2en(self):
+        '''here comes 4 tests 2 for asserteq 2 for assertNoteq'''
+        self.assertNotEqual(french_to_english(0), 0)
+        # test when null is given as input the output is null.
+        self.assertNotEqual(french_to_english('null'), '')
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        # test when word is given as input the output is translated.
+        self.assertEqual(french_to_english('Route'), 'Road')
 
 unittest.main()
